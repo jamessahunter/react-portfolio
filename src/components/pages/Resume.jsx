6f7f3dbeef746aeecbf7c5b93781d97ef66a1073
James Hunter <jamessahunter@gmail.com>
2023-12-01 16:22:03 -0800

    add resume page

---
+++ src/components/pages/Resume.jsx
@@ -0,0 +1,19 @@
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <h2>Front End</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ul>
+      <h2>Back End</h2>
+      <ul>
+        <li>Node</li>
+        <li>Express</li>
+        <li>React</li>
+      </ul>
+    </div>
+  );
+}
